7f0eba16eb25d298c6ed9558b11aedeab511aaec
devcation <88315076+devcation-learn@users.noreply.github.com>
2026-02-26 17:37:02 +0900

    aorjwls

---
+++ src/pages/GalleryPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Typography } from 'antd';
+// import { useArtwork } from '../hooks/useArtwork';
+
+const GalleryPage: React.FC = () => {
+  // const { artworks } = useArtwork();
+  return (
+    <div style={{padding: '40px 0'}}>
+      <Typography.Title level={2} style={{fontFamily: 'serif'}}>THE GALLERY</Typography.Title>
+      <Typography.Paragraph>
+        이곳에 작품 아카이브가 표시됩니다.
+      </Typography.Paragraph>
+    </div>
+  );
+};
+
+export default GalleryPage;
