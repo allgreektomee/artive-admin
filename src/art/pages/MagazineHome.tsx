7e493471c488ddb3d20f1e88aa64c842dc59dd12
devcation <88315076+devcation-learn@users.noreply.github.com>
2026-03-10 15:41:19 +0900

    메거진홈 아트워크

--- src/art/pages/MagazineHome.tsx
+++ src/art/pages/MagazineHome.tsx
@@ -1,24 +1,19 @@
+import React from "react";
+
 const MagazineHome = () => {
   const images = {
-    studioView:
-      "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/studio.png",
-    mentorView:
-      "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/jsh.jpg",
-    work100_1:
-      "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/work100_1.png",
-    work120_process:
-      "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/120_1.png",
     first:
       "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/first.png",
-    love: "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/60.png",
     work1:
       "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/123.png",
     work2:
       "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/456.png",
     work3:
       "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/678.png",
-    work8:
-      "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/120_2.png",
+    work100_1:
+      "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/work100_1.png",
+    studioView:
+      "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/studio.png",
   };
 
   return (
@@ -32,14 +27,14 @@ const MagazineHome = () => {
         fontFamily: '"Noto Serif KR", serif',
       }}
     >
-      {/* 1. BANNER 영역 (Height 확장 + 센터 타이틀 + 반투명 배경) */}
+      {/* 1. BANNER 영역 (Height 70vh + 센터 타이틀) */}
       <section
         style={{
           position: "relative",
           width: "100%",
           height: "70vh",
           overflow: "hidden",
-          marginBottom: "100px",
+          marginBottom: "80px",
         }}
       >
         <img
@@ -47,27 +42,28 @@ const MagazineHome = () => {
           alt="Main Banner"
           style={{ width: "100%", height: "100%", objectFit: "cover" }}
         />
-        {/* 중앙 텍스트 박스 */}
         <div
           style={{
             position: "absolute",
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            backgroundColor: "rgba(0, 0, 0, 0.4)", // 회색 투명값
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
             padding: "40px 60px",
             textAlign: "center",
             minWidth: "60%",
-            backdropFilter: "blur(4px)", // 살짝 블러 줘서 더 고급스럽게
+            backdropFilter: "blur(4px)",
+            borderRadius: "2px",
           }}
         >
           <h1
             style={{
               color: "#fff",
-              fontSize: "32px",
+              fontSize: "28px",
               margin: 0,
               fontWeight: 500,
-              letterSpacing: "2px",
+              letterSpacing: "3px",
+              lineHeight: "1.4",
             }}
           >
             2026 ART BUSAN
@@ -76,18 +72,19 @@ const MagazineHome = () => {
           </h1>
           <p
             style={{
-              color: "#eee",
-              fontSize: "14px",
+              color: "#ccc",
+              fontSize: "13px",
               marginTop: "15px",
               fontWeight: 300,
+              letterSpacing: "1px",
             }}
           >
-            아카이브의 시작, 선과 면의 기록
+            선과 면의 기록, 아카이브의 시작
           </p>
         </div>
       </section>
 
-      {/* 2. ARTWORK 영역 (스와이프 유도 레이아웃) */}
+      {/* 2. ARTWORK 영역 (높이 고정형 스와이프) */}
       <section style={{ marginBottom: "120px" }}>
         <p
           style={{
@@ -95,35 +92,53 @@ const MagazineHome = () => {
             color: "#aaa",
             letterSpacing: "2px",
             paddingLeft: "20px",
-            marginBottom: "20px",
+            marginBottom: "25px",
           }}
         >
           01 SELECTED ARTWORK
         </p>
 
-        {/* 가로 스크롤 컨테이너 */}
         <div
           style={{
             display: "flex",
             overflowX: "auto",
             paddingLeft: "20px",
-            paddingRight: "20px",
-            gap: "20px",
-            scrollbarWidth: "none", // 파이어폭스
-            msOverflowStyle: "none", // IE
+            paddingRight: "40px", // 마지막 여유 공간
+            gap: "15px",
+            scrollbarWidth: "none",
+            msOverflowStyle: "none",
+            alignItems: "flex-start",
           }}
         >
-          {/* 각 아이템 너비를 약 75%로 설정하여 1.3개가 보이도록 유도 */}
+          {/* 높이를 400px로 고정하고 너비는 이미지 비율에 맞춤(auto) */}
           {[images.work1, images.work2, images.work3, images.work100_1].map(
             (img, i) => (
-              <div key={i} style={{ flex: "0 0 75%", minWidth: "75%" }}>
+              <div
+                key={i}
+                style={{
+                  flex: "0 0 auto",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
                 <img
                   src={img}
                   alt={`Work ${i}`}
-                  style={{ width: "100%", height: "auto", display: "block" }}
+                  style={{
+                    height: "400px", // 높이 고정
+                    width: "auto", // 너비는 원본 비율에 따라 가변
+                    display: "block",
+                    objectFit: "contain", // 이미지 잘리지 않게 보호
+                    backgroundColor: "#f9f9f9", // 비율 차이로 생기는 여백 대비
+                  }}
                 />
                 <p
-                  style={{ fontSize: "11px", color: "#999", marginTop: "10px" }}
+                  style={{
+                    fontSize: "11px",
+                    color: "#888",
+                    marginTop: "12px",
+                    textAlign: "left",
+                  }}
                 >
                   Piece No. 0{i + 1}
                 </p>
@@ -131,12 +146,14 @@ const MagazineHome = () => {
             ),
           )}
         </div>
-        <style>{
-          `.container::-webkit-scrollbar { display: none; }` /* 크롬 스크롤바 숨기기 */
-        }</style>
+
+        {/* 스크롤바 숨기기 스타일 (실제 프로젝트 CSS에 추가 권장) */}
+        <style>{`
+          div::-webkit-scrollbar { display: none; }
+        `}</style>
       </section>
 
-      {/* 이후 LOG, INSIGHT 등은 기존과 동일하되 위 섹션들의 변화에 맞춰 여백 조정 */}
+      {/* 3. LOG 영역 (샘플) */}
       <section style={{ padding: "0 20px", marginBottom: "100px" }}>
         <p
           style={{
