b38eafa6d3f5651f9621fe8f0d747ad099c97677
devcation <88315076+devcation-learn@users.noreply.github.com>
2026-03-10 15:44:18 +0900

    아트워크

--- src/art/pages/MagazineHome.tsx
+++ src/art/pages/MagazineHome.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from 'react';
 
 const MagazineHome = () => {
   const images = {
@@ -14,6 +14,13 @@ const MagazineHome = () => {
       "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/work100_1.png",
     studioView:
       "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/studio.png",
+    work120_process:
+      "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/120_1.png",
+    work8:
+      "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/120_2.png",
+    mentorView:
+      "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/jsh.jpg",
+    love: "https://artive-uploads.s3.ap-southeast-2.amazonaws.com/test/60.png",
   };
 
   return (
@@ -27,14 +34,14 @@ const MagazineHome = () => {
         fontFamily: '"Noto Serif KR", serif',
       }}
     >
-      {/* 1. BANNER 영역 (Height 70vh + 센터 타이틀) */}
+      {/* 1. BANNER: 모바일 대응 높이 및 텍스트 박스 */}
       <section
         style={{
           position: "relative",
           width: "100%",
-          height: "70vh",
+          height: "75vh",
           overflow: "hidden",
-          marginBottom: "80px",
+          marginBottom: "60px",
         }}
       >
         <img
@@ -48,33 +55,42 @@ const MagazineHome = () => {
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-            padding: "40px 60px",
+            backgroundColor: "rgba(20, 20, 20, 0.65)",
+            padding: "40px 30px", // 모바일 고려 패딩 축소
             textAlign: "center",
-            minWidth: "60%",
-            backdropFilter: "blur(4px)",
-            borderRadius: "2px",
+            width: "85%", // 모바일에서 박스가 넉넉히 차지하도록
+            maxWidth: "500px",
+            backdropFilter: "blur(10px)",
+            border: "1px solid rgba(255,255,255,0.1)",
+            boxSizing: "border-box",
           }}
         >
           <h1
             style={{
               color: "#fff",
-              fontSize: "28px",
+              fontSize: "1.6rem",
               margin: 0,
               fontWeight: 500,
               letterSpacing: "3px",
-              lineHeight: "1.4",
+              lineHeight: "1.3",
             }}
           >
             2026 ART BUSAN
             <br />
             THE FIRST ARCHIVE
           </h1>
+          <div
+            style={{
+              width: "30px",
+              height: "1px",
+              backgroundColor: "#fff",
+              margin: "15px auto",
+            }}
+          ></div>
           <p
             style={{
               color: "#ccc",
-              fontSize: "13px",
-              marginTop: "15px",
+              fontSize: "12px",
               fontWeight: 300,
               letterSpacing: "1px",
             }}
@@ -84,33 +100,32 @@ const MagazineHome = () => {
         </div>
       </section>
 
-      {/* 2. ARTWORK 영역 (높이 고정형 스와이프) */}
-      <section style={{ marginBottom: "120px" }}>
+      {/* 2. ARTWORK: 가로 스와이프 (높이 고정형) */}
+      <section style={{ marginBottom: "100px" }}>
         <p
           style={{
             fontSize: "10px",
             color: "#aaa",
             letterSpacing: "2px",
             paddingLeft: "20px",
-            marginBottom: "25px",
+            marginBottom: "20px",
           }}
         >
           01 SELECTED ARTWORK
         </p>
 
         <div
+          className="artwork-scroll"
           style={{
             display: "flex",
             overflowX: "auto",
             paddingLeft: "20px",
-            paddingRight: "40px", // 마지막 여유 공간
+            paddingRight: "60px",
             gap: "15px",
-            scrollbarWidth: "none",
-            msOverflowStyle: "none",
-            alignItems: "flex-start",
+            alignItems: "flex-end",
+            WebkitOverflowScrolling: "touch", // iOS 부드러운 스크롤
           }}
         >
-          {/* 높이를 400px로 고정하고 너비는 이미지 비율에 맞춤(auto) */}
           {[images.work1, images.work2, images.work3, images.work100_1].map(
             (img, i) => (
               <div
@@ -124,21 +139,16 @@ const MagazineHome = () => {
                 <img
                   src={img}
                   alt={`Work ${i}`}
+                  className="artwork-img"
                   style={{
-                    height: "400px", // 높이 고정
-                    width: "auto", // 너비는 원본 비율에 따라 가변
+                    height: "380px", // 모바일 가독성을 위해 살짝 조절 (데스크탑에선 미디어쿼리로 키움)
+                    width: "auto",
                     display: "block",
-                    objectFit: "contain", // 이미지 잘리지 않게 보호
-                    backgroundColor: "#f9f9f9", // 비율 차이로 생기는 여백 대비
+                    objectFit: "contain",
                   }}
                 />
                 <p
-                  style={{
-                    fontSize: "11px",
-                    color: "#888",
-                    marginTop: "12px",
-                    textAlign: "left",
-                  }}
+                  style={{ fontSize: "10px", color: "#888", marginTop: "10px" }}
                 >
                   Piece No. 0{i + 1}
                 </p>
@@ -146,37 +156,65 @@ const MagazineHome = () => {
             ),
           )}
         </div>
-
-        {/* 스크롤바 숨기기 스타일 (실제 프로젝트 CSS에 추가 권장) */}
-        <style>{`
-          div::-webkit-scrollbar { display: none; }
-        `}</style>
       </section>
 
-      {/* 3. LOG 영역 (샘플) */}
-      <section style={{ padding: "0 20px", marginBottom: "100px" }}>
+      {/* 3. LOG & OTHERS (중략 - 이전과 동일하되 텍스트 크기 미세 조정) */}
+      <section style={{ padding: "0 20px", marginBottom: "80px" }}>
         <p
           style={{
             fontSize: "10px",
             color: "#aaa",
             letterSpacing: "2px",
-            marginBottom: "40px",
+            marginBottom: "25px",
           }}
         >
           02 LOG: PROCESS
         </p>
-        <div style={{ display: "flex", gap: "30px", alignItems: "flex-start" }}>
-          <div style={{ flex: 4 }}>
-            <img src={images.studioView} alt="Studio" />
-          </div>
-          <div style={{ flex: 6 }}>
-            <h3 style={{ fontSize: "18px", marginTop: 0 }}>작업실 기록</h3>
-            <p style={{ color: "#444", fontSize: "13px" }}>
-              빛이 가장 깊게 들어오는 시간의 기록.
-            </p>
-          </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "2px",
+            marginBottom: "20px",
+          }}
+        >
+          <img src={images.work120_process} style={{ width: "100%" }} />
+          <img src={images.work8} style={{ width: "100%" }} />
         </div>
+        <h3 style={{ fontSize: "18px", fontWeight: 500 }}>
+          대작의 층위: 120호 기록
+        </h3>
+        <p style={{ color: "#444", fontSize: "13px", marginTop: "8px" }}>
+          물감이 쌓이고 긁혀나가는 과정에서 발견된 질감.
+        </p>
       </section>
+
+      {/* 미디어 쿼리 스타일링 */}
+      <style>{`
+        .artwork-scroll::-webkit-scrollbar { display: none; }
+        
+        @media (min-width: 600px) {
+          .artwork-img { height: 450px !important; }
+          h1 { fontSize: '30px' !important; }
+        }
+
+        /* 모바일 텍스트 가독성 */
+        @media (max-width: 480px) {
+          p { font-size: 13px !important; line-height: 1.6 !important; }
+        }
+      `}</style>
+
+      <footer
+        style={{
+          padding: "60px 0",
+          textAlign: "center",
+          borderTop: "1px solid #eee",
+        }}
+      >
+        <p style={{ fontSize: "10px", color: "#bbb", letterSpacing: "2px" }}>
+          © 2026 ARTIVE ARCHIVE
+        </p>
+      </footer>
     </div>
   );
 };
