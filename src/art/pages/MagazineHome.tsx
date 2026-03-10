81860e39f832ba8c628f39711c6b57f3d139e6c4
devcation <88315076+devcation-learn@users.noreply.github.com>
2026-03-10 18:01:43 +0900

    123

--- src/art/pages/MagazineHome.tsx
+++ src/art/pages/MagazineHome.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { useResponsive } from "../hook/useResponsive";
 import ArtworkCard from "../components/ArtworkCard";
 // 새로 만든 컴포넌트들 임포트
