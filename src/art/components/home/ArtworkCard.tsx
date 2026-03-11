bfadd5a668c8d09fad810d528dfdc368e57101d4
devcation <88315076+devcation-learn@users.noreply.github.com>
2026-03-11 17:02:33 +0900

    ArtworkCard

--- src/art/components/home/ArtworkCard.tsx
+++ src/art/components/home/ArtworkCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useResponsive } from "../../hook/useResponsive";
 
 interface ArtworkCardProps {
   imageUrl: string;
@@ -14,7 +13,9 @@ const ArtworkCard: React.FC<ArtworkCardProps> = ({
   artworkInfo,
   artistName,
 }) => {
-  const { isMobile } = useResponsive();
+  // 💡 기기 상관없이 모바일 최적화된 비율로 고정
+  const fixedHeight = "380px";
+  const cardWidth = "280px";
 
   return (
     <div
@@ -22,32 +23,40 @@ const ArtworkCard: React.FC<ArtworkCardProps> = ({
         flex: "0 0 auto",
         display: "flex",
         flexDirection: "column",
-        // 모바일에서도 카드가 너무 작지 않게 너비 확보
-        width: isMobile ? "75vw" : "auto",
-        maxWidth: isMobile ? "320px" : "none",
+        width: cardWidth, // 카드 너비 고정
       }}
     >
-      {/* --- 이미지 영역: 비율 유지하며 시원하게 --- */}
-      <img
-        src={imageUrl}
-        alt={artworkName}
+      {/* --- 이미지 영역: 높이 380px로 완전 고정 --- */}
+      <div
         style={{
-          // 모바일에서 높이를 350px~400px 정도로 넉넉히 주거나 auto로 비율 유지
-          height: isMobile ? "380px" : "420px",
+          height: fixedHeight,
           width: "100%",
-          display: "block",
-          objectFit: "contain", // 이미지 전체가 잘리지 않게
           backgroundColor: "#fcfcfc",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          overflow: "hidden",
         }}
-      />
+      >
+        <img
+          src={imageUrl}
+          alt={artworkName}
+          style={{
+            maxHeight: "100%",
+            maxWidth: "100%",
+            objectFit: "contain", // 어떤 비율이 와도 꽉 차거나 여백 생기며 정렬됨
+            display: "block",
+          }}
+        />
+      </div>
 
-      {/* --- 텍스트 영역 (넘버링 삭제) --- */}
-      <div style={{ marginTop: "16px" }}>
-        {/* 1층: 우측 상단 인포 */}
-        <div style={{ textAlign: "right", marginBottom: "6px" }}>
+      {/* --- 텍스트 영역: 모바일에서 보던 크기 그대로 --- */}
+      <div style={{ marginTop: "14px", paddingRight: "4px" }}>
+        {/* 1층: 우측 상단 인포 (이탤릭) */}
+        <div style={{ textAlign: "right", marginBottom: "4px" }}>
           <p
             style={{
-              fontSize: isMobile ? "11px" : "10px",
+              fontSize: "11px",
               color: "#888",
               fontStyle: "italic",
               margin: 0,
@@ -61,11 +70,11 @@ const ArtworkCard: React.FC<ArtworkCardProps> = ({
         <div style={{ textAlign: "left" }}>
           <h4
             style={{
-              fontSize: isMobile ? "16px" : "14px",
+              fontSize: "15px",
               color: "#1a1a1a",
               fontWeight: 600,
               margin: 0,
-              letterSpacing: "-0.5px",
+              lineHeight: "1.2",
             }}
           >
             {artworkName}{" "}
