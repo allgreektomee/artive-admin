42b4a800828e31a95437e576255eea9348f868bf
devcation <88315076+devcation-learn@users.noreply.github.com>
2026-03-12 18:11:14 +0900

    ㅅㄴㅁㅅ

--- src/art/pages/ArtHome.tsx
+++ src/art/pages/ArtHome.tsx
@@ -10,13 +10,13 @@ const ArtHome = () => {
   return (
     <div className="art-page-root">
       {/* 섹션 1: 메인 배너 */}
-      <MainBanner />
 
       {/* 섹션 2 : artwork */}
       {/* <SelectedWorks isMobile={isMobile} limit={10} /> */}
       <ArtWorksSection />
       {/* 섹션 2: 매거진 리스트 (이런 식으로 확장) */}
       {/* <MagazineList /> */}
+      <MainBanner />
       <InsightSection />
       <LogSection />
     </div>
