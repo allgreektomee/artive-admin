fa230e9230cdb80ba47d19ed589f9a4747d57457
devcation <88315076+devcation-learn@users.noreply.github.com>
2026-03-13 14:14:06 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { registerServices } from "./core/di/registerServices.ts";
+// import { registerServices } from "./core/di/registerServices.ts";
 
 // 애플리케이션 서비스 의존성 등록
-registerServices();
+// registerServices();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
